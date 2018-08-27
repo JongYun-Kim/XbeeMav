4aca4dc0f6b4117cceb0d4a4ec15a09558668779
JongYun-Kim <36770713+JongYun-Kim@users.noreply.github.com>
2018-08-28 02:08:33 +0900

    Add files via upload

--- src/gpr_data_node2.cpp
+++ src/gpr_data_node2.cpp
@@ -13,6 +13,7 @@ using namespace std;
 double file_w_count;   // file write count variable
 //int fw_mav_dummy = 0;  // file wrtie dummy variable
 double fw_rssi1 = 0;  //file write variables
+double fw_rssi2 = 0;
 double fw_pose_x = 0;
 double fw_pose_y = 0;
 
@@ -35,18 +36,24 @@ void msgCallback_rssi1(const std_msgs::Float64::ConstPtr& msg_rssi1)
   fw_rssi1 = msg_rssi1->data;
 }
 
+void msgCallback_rssi2(const std_msgs::Float64::ConstPtr& msg_rssi2)
+{
+  fw_rssi2 = msg_rssi2->data;
+}
+
 // --------------------------------------------------------------------//
 // MAIN function
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "gpr_data_node");
-  if (argc != 3) // input validation
+  ros::init(argc, argv, "gpr_data_node2");
+  if (argc != 4) // input validation
   {
     ROS_INFO("usage: gpr_data_node file_number + rssi_device1_number + rssi_device2_number , PLEASE use the conmmand properly");
     return 1;
   }
   std::string file_num_ = argv[1]; // the input argument : file number in the file name
   std::string short_id1_ = argv[2]; // the input short id : target rssi device short id number (device1)
+  std::string short_id2_ = argv[3]; // the input short id : target rssi device short id number (device2)
 
   ros::NodeHandle nh;
 
@@ -55,20 +62,23 @@ int main(int argc, char **argv)
 
   // Get rssi topic name
   std::string rssi_topic1 = "/rssi_val_" + short_id1_;
+  std::string rssi_topic2 = "/rssi_val_" + short_id2_;
 
   // Subscriber declaration
   ros::Subscriber pose_sub = nh.subscribe("/amcl_pose", 100, msgCallback_pose);
   ros::Subscriber rssi_sub1 = nh.subscribe(rssi_topic1, 100, msgCallback_rssi1);
+  ros::Subscriber rssi_sub2 = nh.subscribe(rssi_topic2, 100, msgCallback_rssi2);
   //ros::Subscriber dummy_sub = nh.subscribe("/inMavlink", 100, msgCallback_dummy);
   ros::Rate loop_rate(3); //S_TIME);
 	while (ros::ok()){
 
     //ROS_INFO("inMavlink msg :  %d ", fw_mav_dummy);
     ROS_INFO("RSSI value 1 : %f  ", fw_rssi1);
+    ROS_INFO("RSSI value 2 : %f  ", fw_rssi2);
     ROS_INFO("AMCL pose  : ( %f , %f ) ", fw_pose_x, fw_pose_y);
 
     // Writing the file
-    file_wrtie_gpr_obj << file_w_count << ", " << fw_pose_x << ", " << fw_pose_y << ", " << fw_rssi1  << endl;
+    file_wrtie_gpr_obj << file_w_count << ", " << fw_pose_x << ", " << fw_pose_y << ", " << fw_rssi1 << ", " << fw_rssi2 << endl;
 
     ros::spinOnce();
     loop_rate.sleep();
@@ -77,4 +87,3 @@ int main(int argc, char **argv)
   file_wrtie_gpr_obj.close();  // may NOT be implemented
   return 0;
 } // end MAIN
-
---
+++ src/rssi_publisher_node2.cpp
@@ -0,0 +1,77 @@
+#include "CommunicationManager.h"
+#include <std_msgs/Float64.h>
+
+
+// MAIN function
+int main(int argc, char **argv)
+{
+  // ROS initialization
+  ros::init(argc, argv, "rssi_publisher_node2");
+  // Input arguments treatment
+  if (argc != 2) // input validation
+  {
+    ROS_INFO("usage: srv_client_node Target_short_ID , PLEASE use the conmmand properly");
+    return 1;
+  }
+  std::string short_id_ = argv[1]; // the input short id
+  //std::string short_id_2 = argv[2]; // the input short id (second device)
+
+
+  ros::NodeHandle nh; // node handle declaration
+
+  // Service client objects(trigger and get) creation
+  ros::ServiceClient client_trig_ = nh.serviceClient<mavros_msgs::ParamGet>("/xbee_status");
+  ros::ServiceClient client_get_ = nh.serviceClient<mavros_msgs::ParamGet>("/xbee_status");
+  // Publisher cliend obj
+  std::string topic_name_ = "rssi_val_" + short_id_;
+  ros::Publisher rssi_val_pub = nh.advertise<std_msgs::Float64>(topic_name_,100);
+
+  // Service messages creation : mavros service format
+  mavros_msgs::ParamGet srv_trig;
+  mavros_msgs::ParamGet srv_get;
+  // Topic message creation : std_msgs UInt8 format
+  std_msgs::Float64 msg;
+
+  // Request contents
+  std::string temp_str1 = "trig_rssi_api_";
+  std::string temp_str2 = "get_rssi_api_";
+  srv_trig.request.param_id = temp_str1 + short_id_;   //"trig_rssi_api_8"; // later, specify the node ID with the argument input
+  srv_get.request.param_id =  temp_str2 + short_id_; //"get_rssi_api_8";
+
+  ros::Rate Loop_Rate(2);  // 5HZ = 0.2s , rate
+  while (ros::ok())
+  {
+    // Call the service : rssi test link trigger
+    if (client_trig_.call(srv_trig))
+    {
+      ROS_INFO("Client_trig_ has been successfully called!!");
+    }
+    else
+    {
+      ROS_ERROR("Failed to call service rssi_trig");
+    }
+
+    // Wait for a while; 1 second
+    ros::Duration(0.1).sleep();
+
+    // Call the serivce : rssi test link access
+//    float the_rssi;
+    if (client_get_.call(srv_get))
+    {
+      ROS_INFO("Client_get_ has been successfully called!!");
+//      the_rssi = srv_get.response.value.real;
+      msg.data = srv_get.response.value.real;
+      printf(" RSSI: %f \n", msg.data);
+      rssi_val_pub.publish(msg);
+    }
+    else
+    {
+      ROS_ERROR("Failed to call service rssi_get");
+      //return 1; Do Not terminate this node although there's no rssi input
+    }
+    ros::spinOnce();
+    Loop_Rate.sleep();
+  }// end while
+
+  return 0;
+} // MAIN end
